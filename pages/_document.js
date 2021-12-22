0bb2f3038df6cf4d0069a755779c2ec0e3c73e0f
rfarrazi <rune.farrazijn@ugent.be>
2021-12-22 22:07:48 +0100

    Aanpassing

--- pages/_document.js
+++ pages/_document.js
@@ -28,7 +28,7 @@ export default class MyDocument extends Document {
           }}
           /> */}
 
-          {/* <script dangerouslySetInnerHTML={{
+          {<script dangerouslySetInnerHTML={{
             __html: `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -38,7 +38,7 @@ export default class MyDocument extends Document {
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
           }}
-          /> */}
+          />}
 
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1446177920850529"
             crossorigin="anonymous"></script>
