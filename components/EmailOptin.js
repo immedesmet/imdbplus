0ff14488364650ee740fc3b21c04617ce5d326b7
rfarrazi <rune.farrazijn@ugent.be>
2021-12-23 18:00:51 +0100

    SendInBlue

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {<iframe width="540" height="305" src="https://1ad00253.sibforms.com/serve/MUIEAI7QpfImfpj0tCP6yELaGBhBRvsPR8uJFjgEztIa2JHVVMXz8NNnkxFXHZ5XP7cjxwf8-MZkZ18s8WppZqFWKGMQhIbkNKaNCGxbXdQszAszZW-Oj9LguLyQksZoIosTpeZ-3xUtt2rI8Syykip8XieQCCzjTCwD_RhRTOpc8ozXEGKLuUHUnHajboo7EZYC8TnWuWuQ19jR" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
+      {formstate&&<iframe width="540" height="900" src="https://1ad00253.sibforms.com/serve/MUIEAI7QpfImfpj0tCP6yELaGBhBRvsPR8uJFjgEztIa2JHVVMXz8NNnkxFXHZ5XP7cjxwf8-MZkZ18s8WppZqFWKGMQhIbkNKaNCGxbXdQszAszZW-Oj9LguLyQksZoIosTpeZ-3xUtt2rI8Syykip8XieQCCzjTCwD_RhRTOpc8ozXEGKLuUHUnHajboo7EZYC8TnWuWuQ19jR" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>}
     </div>
   );
 };
