6051d4d3544575b24e2fe11a79d52eddfa7170b2
rfarrazi <rune.farrazijn@ugent.be>
2022-01-02 12:50:55 +0100

    Aanpassing

--- components/NavigationMobile.js
+++ components/NavigationMobile.js
@@ -34,7 +34,7 @@ const NavigationMobile = ({ locale, locales }) => {
         <div className={styles.navlogo}>
           <a href="/">
             <img
-              src="https://a.storyblok.com/f/133261/3039x582/a60d166ec2/logo-colored-full.png/m/200x0"
+              src="Wanderlust.jpg"
               alt="Wanderlust Logo"
               className=""
             />
